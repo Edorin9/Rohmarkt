b544e80a70c4e6317aef627a5e284787dfadd6ae
Edryn Magcalas <edryn.magcalas@gmail.com>
2021-02-28 23:30:54 +0800

    refactor main dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'common/theme/theme.dart';
-import 'data/services/connection_checker.dart';
-import 'data/sources/dreamshape_api.dart';
-import 'data/sources/json_provider.dart';
+import 'global_dependencies.dart';
 import 'routes/pages.dart';
 
 void main() {
@@ -21,28 +19,8 @@ class Rohmarkt extends GetMaterialApp {
           themeMode: ThemeMode.dark,
           theme: AppTheme.light,
           darkTheme: AppTheme.dark,
-          initialBinding: _InitialBinding(),
+          initialBinding: GlobalDependencies(),
           initialRoute: Routes.market,
           getPages: Pages.routes,
         );
-}
-
-/// + Initial Binding
-
-class _InitialBinding extends Bindings {
-  @override
-  void dependencies() {
-    // + ConnectionChecker
-    Get.lazyPut<ConnectionChecker>(
-      () => ConnectionChecker(),
-    );
-    // + JsonProvider
-    Get.lazyPut<JsonProvider>(
-      () => JsonProvider(),
-    );
-    // + DreamShapeApi
-    Get.lazyPut<DreamShapeApi>(
-      () => DreamShapeApi(),
-    );
-  }
 }
